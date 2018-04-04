bf460cd227dc87657937e9454dab6e57e07337a5
Arthur Michajlenko <michajlenko1967@gmail.com>
2018-04-04 15:24:29 +0300

    Start Config

--- SPBot.go
+++ SPBot.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
 // APIKeys API keys from provider
 type APIKeys struct {
 	TgbAPI string `json:"tgb"`
+}
+
+// LoadAPIKeys load API keys from json file
+func LoadAPIKeys(file string) (APIKeys, error) {
+	var apikeys APIKeys
+	apikeysFile, err := os.Open(file)
+	defer apikeysFile.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	jsonParse := json.NewDecoder(apikeysFile)
+	err = jsonParse.Decode(&apikeys)
+	return apikeys, err
 }
 
 func main() {}
