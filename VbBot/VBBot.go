de6db34e304f1b67b3a330f007cdc06ae8998a4c
Arthur Michajlenko <michajlenko1967@gmail.com>
2018-09-17 11:34:38 +0300

    Viber begin

---
+++ VbBot/VBBot.go
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Arthur Michajlenko
+ *
+ * @Script: VBBot.go
+ * @Author: Arthur Michajlenko
+ * @Email: michajlenko1967@gmail.com
+ * @Create At: 2018-09-17 10:49:45
+ * @Last Modified By: Arthur Michajlenko
+ * @Last Modified At: 2018-09-17 11:26:39
+ * @Description: Viber bot.
+ */
+
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Begin")
+}
