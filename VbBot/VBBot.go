7bd361ff71c363e6fdb383e20b7b403d0f04bc87
Arthur Michajlenko <michajlenko1967@gmail.com>
2018-09-19 14:19:05 +0300

    Set webhook

--- VbBot/VBBot.go
+++ VbBot/VBBot.go
@@ -14,8 +14,36 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/mileusna/viber"
 )
 
 func main() {
 	log.Println("Begin VB")
+	v := viber.New("48799744e527d2c8-d447e6da2f634a31-8a717c2c7cd69d51", "IumasLink", "")
+	vAccount, err := v.AccountInfo()
+	if err != nil {
+		log.Println(err)
+	}
+	v.Sender.Avatar = vAccount.Icon
+	http.Handle("/", v)
+	go http.ListenAndServe("0.0.0.0:8444", nil)
+	webHookResp, err := v.SetWebhook("https://dev.infinitloop.md:8444/", nil)
+	if err != nil {
+		log.Println("WebHook error=> ", err)
+	} else {
+		log.Println("WebHook resp=> ", webHookResp)
+	}
+
+	userID := "bnzFlKadhfEx/nOKdHXrCw==" // fake user ID, use the real one
+	// send text message
+	token, err := v.SendTextMessage(userID, "Hello, World!\nПривет Мир")
+	if err != nil {
+		log.Println("Viber error:", err)
+	} else {
+		log.Println("Message sent, message token:", token)
+	}
+	log.Println(v)
+	log.Println(vAccount)
 }
