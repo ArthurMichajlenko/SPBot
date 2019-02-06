4735e3c4dd5b282193c668c3fab98b0dc1b51304
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-02-06 09:35:37 +0200

    Carousel cont

--- VbBot/VBhelper.go
+++ VbBot/VBhelper.go
@@ -399,7 +399,10 @@ func msgReceived(v *viber.Viber, u viber.User, m viber.Message, token uint64, t 
 		case "top":
 			msg = v.NewTextMessage(txt + stubMsgText)
 			msgURL := v.NewURLMessage("test", "http://esp.md/sobytiya/2019/01/19/v-avarii-u-zavoda-reut-v-belcah-pogib-chelovek")
-			msgCarousel := v.NewRichMediaMessage(6, 6, "#752f35")
+			msgCarousel := v.NewRichMediaMessage(6, 7, "#752f35")
+			msgCarousel.AddButton(v.NewTextButton(6, 2, viber.OpenURL, "http://esp.md/sobytiya/2019/02/03/na-belckom-avtovokzale-evakuirovali-lyudey-policiya-ocepila-perron-obnovleno", "03.02.2019 - 19:05\n"+"На бельцком автовокзале эвакуировали людей. Полиция оцепила перрон (обновлено)"))
+			msgCarousel.AddButton(v.NewImageButton(6, 4, viber.OpenURL, "http://esp.md/sobytiya/2019/02/03/na-belckom-avtovokzale-evakuirovali-lyudey-policiya-ocepila-perron-obnovleno", "http://esp.md/sites/default/files/vokzal-trevoga02.jpg"))
+			msgCarousel.AddButton(v.NewTextButton(6, 1, viber.OpenURL, "http://esp.md/sobytiya/2019/02/03/na-belckom-avtovokzale-evakuirovali-lyudey-policiya-ocepila-perron-obnovleno", "Подробнее..."))
 			v.SendMessage(u.ID, msgURL)
 			v.SendMessage(u.ID, msgCarousel)
 		case "news":
