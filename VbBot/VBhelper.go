e3ef3fb4b29c4993a27f505bb1990732374405ad
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-02-18 11:19:50 +0200

    Begin Bolt

--- VbBot/VBhelper.go
+++ VbBot/VBhelper.go
@@ -14,6 +14,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/asdine/storm"
+
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 	"github.com/jordan-wright/email"
 	"github.com/mileusna/viber"
@@ -128,33 +130,9 @@ type AttachFile struct {
 	BotFile     tgbotapi.File
 }
 
-//TgUser Telegram User. BoltDb
-type TgUser struct {
-	ChatID            int64 `storm:"id"`
-	FirstName         string
-	LastName          string
-	Username          string `storm:"unique"`
-	LastDate          int
-	Subscribe9        bool
-	Subscribe20       bool
-	SubscribeLast     bool
-	SubscribeCity     bool
-	SubscribeTop      bool
-	SubscribeHolidays bool
-	RssLastID         int
-}
-
-//TgMessageOwner info about who send message.
-type TgMessageOwner struct {
-	ID        string
-	Username  string
-	FirstName string
-	LastName  string
-}
-
 //VbUser Viber User. BoltDb
 type VbUser struct {
-	ID                int
+	ID                int    `storm:"increment"`
 	UserID            string `storm:"unique"`
 	Username          string
 	LastDate          int
@@ -239,55 +217,6 @@ func LoadConfigBots(file string) (Config, error) {
 		log.Panic(err)
 	}
 	return botsconfig, err
-}
-
-//SubButtons create keyboard for subscriptions.
-func SubButtons(update *tgbotapi.Update, user *TgUser) tgbotapi.EditMessageReplyMarkupConfig {
-	bt9 := "Утром"
-	bt20 := "Вечером"
-	btL := "Последние новости"
-	btT := "Самое популярное"
-	btC := "Городские уведомления"
-	btH := "Календарь праздников"
-	btF := "Продолжить..."
-	if user.Subscribe9 {
-		bt9 = "\u2705" + bt9
-	}
-	if user.Subscribe20 {
-		bt20 = "\u2705" + bt20
-	}
-	if user.SubscribeLast {
-		btL = "\u2705" + btL
-	}
-	if user.SubscribeTop {
-		btT = "\u2705" + btT
-	}
-	if user.SubscribeCity {
-		btC = "\u2705" + btC
-	}
-	if user.SubscribeHolidays {
-		btH = "\u2705" + btH
-	}
-	buttonSubscribe9 := tgbotapi.NewInlineKeyboardButtonData(bt9, "subscribe9")
-	buttonSubscribe20 := tgbotapi.NewInlineKeyboardButtonData(bt20, "subscribe20")
-	buttonSubscribeLast := tgbotapi.NewInlineKeyboardButtonData(btL, "subscribelast")
-	buttonSubscribeTop := tgbotapi.NewInlineKeyboardButtonData(btT, "subscribetop")
-	buttonSubscribeCity := tgbotapi.NewInlineKeyboardButtonData(btC, "subscribecity")
-	buttonSubscribeHolidays := tgbotapi.NewInlineKeyboardButtonData(btH, "subscribeholidays")
-	buttonSubscribeFinish := tgbotapi.NewInlineKeyboardButtonData(btF, "subscribefinish")
-	var row0 []tgbotapi.InlineKeyboardButton
-	var row1 []tgbotapi.InlineKeyboardButton
-	var row2 []tgbotapi.InlineKeyboardButton
-	var row3 []tgbotapi.InlineKeyboardButton
-	row0 = append(row0, buttonSubscribe9)
-	row0 = append(row0, buttonSubscribe20)
-	row1 = append(row1, buttonSubscribeLast)
-	row1 = append(row1, buttonSubscribeTop)
-	row2 = append(row2, buttonSubscribeCity)
-	row2 = append(row2, buttonSubscribeHolidays)
-	row3 = append(row3, buttonSubscribeFinish)
-	keyboard := tgbotapi.NewInlineKeyboardMarkup(row0, row1, row2, row3)
-	return tgbotapi.NewEditMessageReplyMarkup(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, keyboard)
 }
 
 // NewsQuery get Nodes from esp.md. -1 without page
@@ -410,6 +339,15 @@ func msgReceived(v *viber.Viber, u viber.User, m viber.Message, token uint64, t 
 	kbMain.AddButton(btFeedback)
 	kbMain.AddButton(btDonate)
 	kbMain.AddButton(btHelp)
+	//Bolt
+	var vbbuser VbUser
+	db, err := storm.Open("vbuser.db")
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+	db.Init(&vbbuser)
+	//Received messages loop
 	switch m.(type) {
 	case *viber.TextMessage:
 		msg := v.NewTextMessage("")
@@ -704,7 +642,7 @@ func msgReceived(v *viber.Viber, u viber.User, m viber.Message, token uint64, t 
 			kb.AddButton(v.NewTextButton(6, 1, viber.Reply, "menu", `<font color="#ffffff">Главное меню</font>`).SetBgColor(spColorBG).SetSilent())
 			msg.SetKeyboard(kb)
 			v.SendMessage(u.ID, msg)
-		case "hi", "hello", "хай", "привет", "рш", "руддщ", "menu", "меню":
+		case "hi", "hello", "хай", "привет", "рш", "руддщ", "menu", "ьутг", "меню":
 			msg = v.NewTextMessage("Выберете комманду")
 			msg.SetKeyboard(kbMain)
 			v.SendMessage(u.ID, msg)
