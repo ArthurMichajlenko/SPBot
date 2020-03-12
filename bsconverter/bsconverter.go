4de8e6434ac7710ba92bd686e65c97549bad4561
Arthur Michajlenko <michajlenko1967@gmail.com>
2020-03-12 14:18:27 +0200

    add go modules

--- bsconverter/bsconverter.go
+++ bsconverter/bsconverter.go
@@ -1,7 +1,63 @@
 package main
 
-import "log"
+import (
+	"github.com/asdine/storm"
+	"log"
+	"time"
+)
+
+//VbUser ...
+type VbUser struct {
+	ID                string
+	Username          string
+	LastDate          time.Time
+	Subscribe9        bool
+	Subscribe20       bool
+	SubscribeLast     bool
+	SubscribeCity     bool
+	SubscribeTop      bool
+	SubscribeHolidays bool
+}
+
+//TgUser ...
+type TgUser struct {
+	ChatID            int64
+	FirstName         string
+	LastName          string
+	Username          string
+	LastDate          int
+	Subscribe9        bool
+	Subscribe20       bool
+	SubscribeLast     bool
+	SubscribeCity     bool
+	SubscribeTop      bool
+	SubscribeHolidays bool
+	RssLastID         int
+}
 
 func main() {
-	log.Println("Hello World!")
+	var tgusers []TgUser
+	var vbusers []VbUser
+	log.Println("Start...")
+	dbTg, err := storm.Open("tguser.db")
+	if err != nil {
+		log.Println(err)
+	}
+	dbVb, err := storm.Open("vbuser.db")
+	if err != nil {
+		log.Println(err)
+	}
+	defer dbTg.Close()
+	defer dbVb.Close()
+	err = dbTg.All(&tgusers)
+	if err != nil {
+		log.Println(err)
+	}
+	err = dbVb.All(&vbusers)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(tgusers)
+	log.Println(vbusers)
+
 }
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/ArthurMichajlenko/SPBot
+
+go 1.14
+
+require (
+	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/Sereal/Sereal v0.0.0-20200210135736-180ff2394e8a // indirect
+	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible
+	github.com/asdine/storm v2.1.2+incompatible
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/golang/protobuf v1.3.4 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jordan-wright/email v0.0.0-20200307200233-de844847de93
+	github.com/mileusna/viber v0.0.0-20180604154358-b816488ead81
+	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	go.etcd.io/bbolt v1.3.3 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/robfig/cron.v2 v2.0.0-20150107220207-be2e0b0deed5
+)
